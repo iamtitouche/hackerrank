319e34a098fcc87c8df53747b2973f50ce063e1b
titouan <titouan.sadoun2002@gmail.com>
2023-10-07 16:03:38 +0200

    Ajout du Challenge N°8 terminé

--- euler_project/c/tests/challenge_008.c
+++ euler_project/c/tests/challenge_008.c
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("One int argument needed smaller than 50");
+		return EXIT_FAILURE;
+	}
+	int n = atoi(argv[1]);
 	int n_lines = 20;
+	int n_columns = 50;
 
 	FILE *fptr;
 	fptr = fopen("misc/challenge_008.txt","r");
@@ -17,12 +23,22 @@ int main() {
 	for (int i = 0; i < n_lines; i++) {
 		*(lines+i) = malloc(50 * sizeof(char));
 		fscanf(fptr, "%s\n", *(lines+i));
-
 	}
 
-	printf("%c\n", *(*(lines + 2) + 4));
+	unsigned long max = 0;
 
+	for (int i = 0; i < n_lines * n_columns - n; i++) {
+		unsigned long product = 1;
+		for (int x = i; x < i + n; x++) {
+			char new_digit = (char) *(*(lines + x / n_columns) + x % n_columns);
+			product *= atoi(&new_digit);
+		}
+		if (product > max) {
+			max = product;
+		}
+	}
 
+	printf("Answer  : %ld\n", max);
 
 	return EXIT_SUCCESS;
 }
