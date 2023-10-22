c3afddfedfb95dcf01e15b91fb1a20e01ac923ea
titouan <titouan.sadoun2002@gmail.com>
2023-10-22 10:30:12 +0200

    Ajout du Challenge N°22 terminé

--- euler_project/c/tests/challenge_022.c
+++ euler_project/c/tests/challenge_022.c
@@ -4,54 +4,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+
+int score(char *name) {
+	int i = 0;
+	int score = 0;
+	while (*(name + i) != '\0') {
+		score += (int) *(name + i) - 64;
+		i++;
+	}
+	return score;
+}
 
 
 int main() {
-	int n = 1, total = 0;
-
 	FILE *fptr;
 	fptr = fopen("misc/challenge_022.txt","r");
 
-	size_t size = 5000;
+	size_t size = 6000;
 	char *names[size];
-	names[0] = malloc(sizeof(char[30]));
+	names[0] = malloc(sizeof(char));
 
 	int i = 0;
-	while (fscanf(fptr, "\"%s,\" ", names[i]) == 1) {
-
-		printf("%s", names[i]);
-
+	while (fscanf(fptr, "\"%[ABCDEFGHIJKLMNOPQRSTUVWXYZ]\", ", names[i]) == 1) {
 		i++;
-		if (i == size) {
-			size += 5000;
-			realloc(names, size);
-		}
 		*(names + i) = malloc(sizeof(char));
 	}
 
+	fclose(fptr);
 
-	char c;
-	fscanf(fptr, "%c", &c);
-
-	while (c == '\"') {
-		int score = 0;
-		printf("%i : ", n);
-		fscanf(fptr, "%c", &c);
-		while (c != '\"' && c != '\0') {
-			printf("%c", c);
-			score += (int) c - 64;
-			fscanf(fptr, "%c", &c);
+	for (int a = 0; a < i - 1; a++) {
+		for (int b = 0; b < i - a - 1; b++) {
+			if (strcmp(names[b], names[b + 1]) > 0) {
+				char *tmp = names[b];
+				names[b] = names[b + 1];
+				names[b + 1] = tmp;
+			}
 		}
-		total += score * n;
-		n++;
-		printf(" %i\n", score);
-		if (fscanf(fptr, ", %c", &c) != 1) {
-			break;
-		};
-
 	}
 
-	fclose(fptr);
+	int total = 0;
+
+	for (int j = 0; j < i; j++) {
+		total += score(names[j]) * (j + 1);
+	}
+
 
 	printf("Answer : %i", total);
 
